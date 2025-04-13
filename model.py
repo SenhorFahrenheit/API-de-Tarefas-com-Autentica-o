8f32c0f22e904ce40c3d217303888c2994109d8a
Yuri Ferreira <yurifronaldo.com>
2025-04-13 12:46:36 -0300

    feat: define molde do banco e implementa criação de tabelas

---
+++ model.py
@@ -0,0 +1,26 @@
+import sqlalchemy as db
+from sqlalchemy import ForeignKey, Table, Column, Boolean, Integer, String, MetaData
+engine = db.create_engine("sqlite:///API-Tarefas.db")
+connection = engine.connect()
+metadata_obj = MetaData()
+
+user_table = Table(
+    "user_table",
+    metadata_obj,
+    Column("id", Integer, primary_key=True),
+    Column("username", String(40), unique=True),
+    Column("password_hash", String(128), nullable=False)
+
+)
+tasks_table = Table(
+    "tasks_table",
+    metadata_obj,
+    Column("id", Integer, primary_key=True),
+    Column("title", String(40), nullable=False),
+    Column("description", String(128), nullable=False),
+    Column("done", Boolean, nullable=False),
+    Column("user_id", ForeignKey("user_table.id"), nullable=False)
+
+)
+
+metadata_obj.create_all(engine)
